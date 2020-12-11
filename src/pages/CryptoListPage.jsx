f32234e14bb65a783675cb6dc937c532edc2a275
Nina <ninahovinmaa@gmail.com>
2020-12-11 14:03:13 +0100

    add cryptoListPage

--- src/pages/CryptoListPage.jsx
+++ src/pages/CryptoListPage.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function CryptoListPage() {
+  const [cryptoList, setCryptoList] = useState(null);
+
+  useEffect(() => {
+    const url = "https://market-data-collector.firebaseio.com/market-collector/crypto.json"
+    fetch(url)
+      .then(res => res.json())
+      .then(data => setCryptoList(data))
+  }, [])
+
+
   return (
     <div>
       <h2>CryptoListPage</h2>
+      {!cryptoList && <p>Loading</p>}
+      {cryptoList && Object.entries(cryptoList).map(cryptoItem => {
+        console.log(cryptoItem[1])
+      })}
     </div>
   );
 }
