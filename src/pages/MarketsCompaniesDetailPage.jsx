9848e99e81ef10c3babd119775cff9272d4d8f25
Georgios <test@example.com>
2020-12-15 13:16:16 +0100

    add graph and Buy/sell buttons. Some styling.

--- src/pages/MarketsCompaniesDetailPage.jsx
+++ src/pages/MarketsCompaniesDetailPage.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import graph from '../pictures/graph.PNG';
+
 
 
 export default function MarketsCompaniesDetailPage(props) {
@@ -19,15 +21,24 @@ export default function MarketsCompaniesDetailPage(props) {
             {!companiesItem && <p>Loading</p>}
             {companiesItem &&
                 <div className="container shadow w-50 mt-5">
-                    <h2>{companiesItem.name}</h2>
-                    <p>Market: {companiesItem.market}</p>
-                    <p>Mtd: {companiesItem.mtd}</p>
-                    <p>Price: {companiesItem.price}</p>
-                    <p>Today: {companiesItem.today}</p>
-                    <p>W1: {companiesItem.w1}</p>
-                    <p>Y3: {companiesItem.y3}</p>
-                    <p>Y5: {companiesItem.y5}</p>
-                    <p>Ytd: {companiesItem.ytd}</p>
+                    <div className ="row">
+                        <div className="col-sm">
+                            <h2>{companiesItem.name}</h2>
+                            <p>Market: {companiesItem.market}</p>
+                            <p>Mtd: {companiesItem.mtd}</p>
+                            <p>Price: {companiesItem.price}</p>
+                            <p>Today: {companiesItem.today}</p>
+                            <p>W1: {companiesItem.w1}</p>
+                            <p>Y3: {companiesItem.y3}</p>
+                            <p>Y5: {companiesItem.y5}</p>
+                            <p>Ytd: {companiesItem.ytd}</p>
+                        </div>
+                        <div className ="col-sm mt-4">
+                            <img src={graph} alt='import'width="304" height="236"/>
+                            <button type="button" class="btn btn-success btn-sm pl-4 pr-4 mt-5 ml-5">Buy</button>
+                            <button type="button" class="btn btn-danger btn-sm pl-4 pr-4 mt-5 ml-4">Sell</button>
+                        </div>  
+                    </div>
                 </div>
             }
 
