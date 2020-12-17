b893c99523ce3fd944175d2c9d678b294d3e8aa7
Robin Hedlund <robin.live@live.se>
2020-12-17 09:21:37 +0100

    add styling to home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,40 +1,68 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+import stats from '../stats.png'
+import bank from '../bank.png'
+import bars from '../bars.png'
+import money from '../money.png'
+
+const Hoover = styled.div`
+  &:hover {
+    background-color: #03c181;
+    p {
+      color: white;
+      font-weight: bold;
+    }
+  }
+`;
 
 export default function HomePage() {
   return (
-    <div className="container vh-100 col-md-6 mt-5 text-center">
-      <div className="row m-5 p-5 justify-content-center">
+    <div className="vh-100">
+      <div className="row justify-content-center mt-5 ">
         <h1 classname="">Stock market</h1>
       </div>
-      <div className="row align-items-center">
-        <div className="card col shadow m-1 p-1">
-          <div className="card-body card-text-center">
-            <Link to="/crypto">Crypto</Link>
-          </div>
+
+      <div className="container  col-md-12 text-center">
+
+        <div className="row align-items-center justify-content-center h-60">
+
+          <Hoover className="card col col-md-2 shadow m-1 p-1">
+            <Link to="/crypto" className="text-reset text-decoration-none">
+              <div className="card-body card-text-center">
+                <img className="img-fluid w-25" src={bank} alt="this is an img" />
+                <p className="font-weight-bold mt-2">Crypto</p>
+              </div>
+            </Link>
+          </Hoover>
+
+          <Hoover className="card col col-md-2 shadow m-1 p-1">
+            <Link to="/currencies" className="text-reset text-decoration-none">
+              <div className="card-body">
+                <img className="img-fluid w-25" src={bars} alt="this is an img" />
+                <p className="font-weight-bold mt-2">Currencies</p>
+              </div>
+            </Link>
+          </Hoover>
+
+          <Hoover className="card col col-md-2 shadow m-1 p-1">
+            <Link to="/indexes" className="text-reset text-decoration-none">
+              <div className="card-body">
+                <img className="img-fluid w-25" src={stats} alt="this is an img" />
+                <p className="font-weight-bold mt-2">Indexes</p>
+              </div>
+            </Link>
+          </Hoover>
+
+          <Hoover className="card col col-md-2 shadow m-1 p-1">
+            <Link to="/markets" className="text-reset text-decoration-none">
+              <div className="card-body">
+                <img className="img-fluid w-25" src={money} alt="this is an img" />
+                <p className="font-weight-bold mt-2">Markets</p>
+              </div>
+            </Link>
+          </Hoover>
         </div>
-        <div className="card col shadow m-1 p-1">
-          <div className="card-body">
-            <Link to="/currencies">Currencies</Link>
-          </div>
-        </div>
-        <div className="card col shadow m-1 p-1">
-          <div className="card-body">
-            <Link to="/indexes">Indexes</Link>
-          </div>
-        </div>
-        <div className="card col shadow m-1 p-1">
-          <div className="card-body">
-            <Link to="/markets">Markets</Link>
-          </div>
-        </div>
-      </div>
-      <div classname="d-flex align-items-end">
-        <footer class="page-footer font-small blue">
-          <div class="footer-copyright text-center py-3">
-            © 2020 Copyright: Group 5
-          </div>
-        </footer>
       </div>
     </div>
   );
